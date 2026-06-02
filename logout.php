cb13ebef2a7ff30bd4d2f4ed4a3f1328a5d135e3
andregoudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-06-02 10:14:49 -0300

    add - adicionei comentários explicativos no arquivo logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,13 @@
 <?php
+
+// Inicia a sessão para poder executá-la
 session_start();
+// Encerra a sessão
+// Remove os dados armazenados da sessão
 session_destroy();
 
+// Redireciona o usuário para a tela de login
 header("Location:index.php");
-exit();
+// Exit impede que o restante do código seja executado após o redirecionamento
 
 ?>
